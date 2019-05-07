501ade57bc9ffb4d7f1c62568b311224bcbc6534
Zach <fiddlezt@gmail.com>
2019-05-07 16:17:07 -0300

    Test Some edits

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -55,14 +55,13 @@ namespace ConsoleApp1
 
 
 
-
+            Console.Write("hello");
 
 
 
 
 
 			Console.WriteLine("what is Kevin's Golden Rule?");
-			Console.ReadKey();
 			Console.WriteLine("Don't put evidence that you're an idiot on the internet");
 			Console.ReadKey();
 		}
